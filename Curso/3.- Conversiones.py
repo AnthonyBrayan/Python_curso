a08d4f782088d538993f9691998b43dedea2c59a
Anthony Brayan <brayan.farronan@gmail.com>
2023-12-27 15:01:13 +0100

    Conversiones

---
+++ Curso/3.- Conversiones.py
@@ -0,0 +1,28 @@
+numero1= "38"
+numero2="25"
+
+print(numero1+numero2)
+
+
+num1 =38
+num2=25
+print(num1+num2)
+
+sueldo = 1200.43
+sueldoEntero= int(sueldo)
+print(sueldoEntero)
+
+valor= "45000.89"
+valorDecimal= float(valor)
+print(valorDecimal)
+
+edad=100
+print(len(str(edad)))
+
+#str(edad) está convirtiendo el número entero 100 a cadena de texto "100"
+"""
+Utilizamos len() para saber cuanto es su lingitud (saber cuantos caracteres tiene).
+100 es un número entero donde la variable edad lo está almacenando.
+si utilizamos solo print(len(edad)) nos dará un error, por que un número no tiene longitud, por lo que se debe convertir a un string (cadena de texto)
+para poder saber la longitud print(len(str(edad)))
+"""
