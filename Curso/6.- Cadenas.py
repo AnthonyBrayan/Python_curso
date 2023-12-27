f907d33e8b89fabc721480017b38f0caff04b200
Anthony Brayan <brayan.farronan@gmail.com>
2023-12-27 16:27:20 +0100

    Cadenas

---
+++ Curso/6.- Cadenas.py
@@ -0,0 +1,12 @@
+texto = "Bienvenidos al curso Python"
+
+print(texto)
+print(texto.lower())
+print(texto.upper())
+print(texto.title())
+print(texto.find("al")) #Posición donde se encuentra la cadena o porción
+print(texto.count("i")) #Cuenta cuantas veces se repite la letra
+
+textoFinal= texto.replace("e","3")
+print(textoFinal)
+
